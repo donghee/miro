c129ffcf018614a011ef592312173b60d89188b4
Donghee <dongheepark@gmail.com>
2011-12-10 00:46:13 +0900

    컬럼값 수정

--- python/shout/read_mic_level.py
+++ python/shout/read_mic_level.py
@@ -1,8 +1,9 @@
 import gst, pygst, struct
 from simpleOSC import *
-
+ 
 (pmin, pmax) = (None, None)
-
+ 
+# gboolean message_handler (GstBus * bus, GstMessage * message, gpointer data)
 def listener_on_message (bus, message, data):
   global pmin, pmax
   s = message.structure
@@ -11,27 +12,25 @@ def listener_on_message (bus, message, data):
     p = rms[0]
     if (pmin==None or p<pmin): pmin = p
     if (pmax==None or p>pmax): pmax = p
-#    print "/effect/shout 0 %s"% (int(p) + 30)
     if (-30 < p):
-      print "level:", p
-      print "/effect/shout 0 %s"% (int(p) + 30)
-      initOSCClient('192.168.0.108', 9000)
-      sendOSCMsg( '/effect/shout', ['0',str(int(p)+30)] )
+	print "level:", p
+    	print "/effect/shout 0 %s 0"% (int(p) + 30)
+    	initOSCClient('10.20.40.154', 9000)
+    	sendOSCMsg( '/effect/shout', ['0',str(int(p)+30)] )
   return True
-
-#listener_desc = 'alsasrc ! level ! fakesink'
-listener_desc = 'pulsesrc ! level ! fakesink'
+ 
+listener_desc = 'alsasrc ! level ! fakesink'
 listener = gst.parse_launch(listener_desc)
 listener.get_bus().add_watch(listener_on_message, None)
-
+ 
 import gobject
 mainloop = gobject.MainLoop()
 listener.set_state(gst.STATE_PLAYING)
-
+ 
 try:
-  mainloop.run()
+	mainloop.run()
 except: # an interruption from Ctrl-C
-  print "stopping"
-
+	print "stopping"
+ 
 # cleanup
 listener.set_state(gst.STATE_NULL)
--- python/shout/shout.sh
+++ python/shout/shout.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/usr/bin/python read_mic_level.py
+cd /home/root/shout
+/usr/bin/python shout.py
